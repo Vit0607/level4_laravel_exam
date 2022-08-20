8d172a71cf7b6809514e6d8d78ab4646e72dc3b3
Vitaly Yurkin <jvn9405@gmail.com>
2022-08-20 18:22:26 +0300

    third commit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,1 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <meta name="description" content="Chartist.html">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+    <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+    <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+    <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+</head>
+<body class="mod-bg-1 mod-nav-link">
+
+@yield('content')
+
+</body>
+
+<script src="js/vendors.bundle.js"></script>
+<script src="js/app.bundle.js"></script>
+<script>
+
+    $(document).ready(function()
+    {
+
+        $('input[type=radio][name=contactview]').change(function()
+        {
+            if (this.value == 'grid')
+            {
+                $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-g');
+                $('#js-contacts .col-xl-12').removeClassPrefix('col-xl-').addClass('col-xl-4');
+                $('#js-contacts .js-expand-btn').addClass('d-none');
+                $('#js-contacts .card-body + .card-body').addClass('show');
+
+            }
+            else if (this.value == 'table')
+            {
+                $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-1');
+                $('#js-contacts .col-xl-4').removeClassPrefix('col-xl-').addClass('col-xl-12');
+                $('#js-contacts .js-expand-btn').removeClass('d-none');
+                $('#js-contacts .card-body + .card-body').removeClass('show');
+            }
+
+        });
+
+        //initialize filter
+        initApp.listFilter($('#js-contacts'), $('#js-filter-contacts'));
+    });
+
+</script>
+</html>
